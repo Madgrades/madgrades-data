53867882160e5a73b8839968cbe810fd1c089f2f
Keenan Thompson <keenan@keenant.com>
2022-11-23 22:36:54 -0800

    Add build action (#1)

---
+++ docker-build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Install dependencies.
+apt-get update
+apt-get install -y \
+  openjdk-17-jdk \
+  git \
+  maven
+# Build madgrades-extractor executable.
+git clone \
+  https://github.com/Madgrades/madgrades-extractor \
+  madgrades-extractor
+cd madgrades-extractor
+mvn install
+cd ..
+# Run madgrades-extractor on the reports.
+java -jar -Xss1m \
+  madgrades-extractor/target/madgrades-final*.jar \
+  -reports ./src \
+  -out ./tables \
+  -f MYSQL \
+  -t 1222
