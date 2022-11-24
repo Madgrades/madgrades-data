e1e73abfb8083116bc72a9107a3772d29ea42c43
Keenan Thompson <keenan@keenant.com>
2022-11-23 23:24:00 -0800

    6m

--- docker-build.sh
+++ docker-build.sh
@@ -14,7 +14,7 @@ mvn install
 cd ..
 # Run madgrades-extractor on the reports.
 java -jar \
-  -Xmx7m \
+  -Xmx6m \
   madgrades-extractor/target/madgrades-final*.jar \
   -reports ./src \
   -out ./tables \
